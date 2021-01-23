eaf1d3922d84f8f957b0df2a9ff779e4175ba840
Shidei Chis <shidei.chis@gmail.com>
2021-01-23 16:40:06 +0700

    add validation

--- src/pages/App.js
+++ src/pages/App.js
@@ -12,8 +12,40 @@ const App = () => {
     const [ overtimePay, setOvertimePay ] = useState(0);
     const [ show, setShow ] = useState(false);
     const [ total, setTotal ] = useState(0);
+    const [ errors, setErrors ] = useState({
+        start: '',
+        end: '',
+        overtime: '',
+        pay: '',
+        overtimePay: ''
+    });
+
+    const validate = () => {
+        const reg = {
+            number: '^[1-9][0-9]+$',
+            time: '^[0-2][0-9]{2}$',
+        };
+        const regexNumber = new RegExp(reg.number);
+        const regexTime = new RegExp(reg.time);
+        if(!regexNumber.test(pay)) {
+            setErrors({ ...errors, pay: 'Input must be number' })
+        }
+        if(!regexNumber.test(overtimePay)) {
+            setErrors({ ...errors, overtimePay: 'Input must be number' })
+        }
+        if(!regexNumber.test(overtimeLimit)) {
+            setErrors({ ...errors, overtime: 'Input must be number' })
+        }
+        if(!regexTime.test(start)) {
+            setErrors({ ...errors, start: 'Invalid time input' })
+        }
+        if(!regexTime.test(end)) {
+            setErrors({ ...errors, start: 'Invalid time input' })
+        }
+    };
 
     const submitData = e => {
+        validate();
         let ttl = total;
         e.preventDefault();
         const startTime = moment(`${date} ${start}`);
@@ -21,10 +53,8 @@ const App = () => {
         const diff = Math.ceil( endTime.diff(startTime, 'hours') );
         if(diff > overtimeLimit) {
             ttl += (diff - overtimeLimit) * overtimePay;
-            console.log('ttl', ttl);
         }
         ttl += (pay * overtimeLimit);
-        console.log('ttl', ttl);
         setTotal(ttl);
     };
 
@@ -34,22 +64,27 @@ const App = () => {
                 <div>
                     <label>Start Time</label>
                     <TimePicker value={start} onChange={setStart} />
+                    <span>{ errors.start }</span>
                 </div>
                 <div>
                     <label>End Time</label>
                     <TimePicker value={end} onChange={setEnd} />
+                    <span>{ errors.end }</span>
                 </div>
                 <div>
                     <label>Overtime Hour Limit</label>
                     <input value={overtimeLimit} onChange={e => setOvertimeLimit(e.target.value)}/>
+                    <span>{ errors.overtime }</span>
                 </div>
                 <div>
                     <label>Basic Pay Rate</label>
                     <input value={pay} onChange={e => setPay(e.target.value)}/>
+                    <span>{ errors.pay }</span>
                 </div>
                 <div>
                     <label>Overtime Pay Rate</label>
                     <input value={overtimePay} onChange={e => setOvertimePay(e.target.value)}/>
+                    <span>{ errors.overtimePay }</span>
                 </div>
                 <div>
                     <button type="submit" onClick={() => setShow(true)}>Show your working</button>
